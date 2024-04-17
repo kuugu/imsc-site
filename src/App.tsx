885ae6a25a9f7705d26a6df37bde305d39851bea
kuugu <anupnavin97@gmail.com>
2024-04-18 00:55:39 +0530

    changed locations for all the images

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import Navbar from './components/Navbar'
 
+// NOTE: for vercel deployment, static files need to be in /public folder 
+//        and the locations need to be relative to that  
+
 function App() {
   return (
     <div>
       <Navbar />
       <img src="/images/mains_2023.jpg"></img>
-      <img src="/public/images/mains_2022.jpg"></img>
-      <img src="/public/images/neet_2022.jpg"></img>
-      <img src="/public/images/kcet_2022.jpg"></img> 
+      <img src="/images/mains_2022.jpg"></img>
+      <img src="/images/neet_2022.jpg"></img>
+      <img src="/images/kcet_2022.jpg"></img> 
     </div>
   )
 }
