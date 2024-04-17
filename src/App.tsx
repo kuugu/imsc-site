beabba93288b0f2a547c6fc884f849501163c173
kuugu <anupnavin97@gmail.com>
2024-04-18 00:46:09 +0530

    moved the static images to public

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,10 @@ function App() {
   return (
     <div>
       <Navbar />
-      <img src="src/assets/315232140_499767582196555_4987720873666681105_n.jpg"></img>
-      <img src="src/assets/315300071_499761982197115_7464690866048234305_n.jpg"></img>
-      <img src="src/assets/315313693_499761588863821_3564976325985916221_n.jpg"></img>
-      <img src="src/assets/329753769_5858769554206939_8220253469491179241_n.jpg"></img> 
+      <img src="public/images/mains_2023.jpg"></img>
+      <img src="public/images/mains_2022.jpg"></img>
+      <img src="public/images/neet_2022.jpg"></img>
+      <img src="public/images/kcet_2022.jpg"></img> 
     </div>
   )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,8 @@
 function Navbar() {
   return (
     <nav className="sticky top-0 px-3 py-3 bg-white shadow-md">
-          {/* <div className="flex justify-between"> */}
           <div className="flex justify-between items-center">
-              <div><img className="h-20 w-22 object-fit shadow-md" src="src/assets/icon3.png"></img></div>
+              <div><img className="h-20 w-22 object-fit shadow-md" src="public/images/icon3.png"></img></div>
               <div className="w-60"></div>
               <div className=""><button className="bg-lgpurple-500 rounded-md text-white px-3 py-3 font-semibold">Programs</button></div>
               <div className=""><button className="bg-lgpurple-500 rounded-md text-white px-3 py-3 font-semibold">Testimonials</button></div>
